38732054095b3e0482b40efade7aba0e710cfa70
Jayant9917 <ranajayant527@gmail.com>
2025-06-29 00:58:37 +0530

    blog card structure finished

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,8 +9,10 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>
         <img onClick={()=>navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44'/>
-        <button onClick={()=>navigate('/Admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-5  py-2.5'>Admin Login
-            <img src={assets.arrow} alt="arrow" className='w-3' />
+        <button onClick={()=>navigate('/Admin')} className='flex items-center gap-2 rounded-full 
+        text-sm cursor-pointer bg-primary text-white px-5  py-2.5'>
+          Admin Login
+          <img src={assets.arrow} alt="arrow" className='w-3' />
         </button>
     </div>
   )
